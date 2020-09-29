ade9e6088d6ad58ba0309e0ed10ef40db45c8894
Yutsevich Anastasia <yutsevicha@ gmail.com>
2020-09-29 16:11:52 +0300

    made hashing algorithm name a constant and added exception message

--- src/main/java/com/internet/shop/util/HashUtil.java
+++ src/main/java/com/internet/shop/util/HashUtil.java
@@ -5,6 +5,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class HashUtil {
+    private static final String HASHING_ALGORITHM = "SHA-512";
 
     public static byte[] getSalt() {
         SecureRandom random = new SecureRandom();
@@ -16,13 +17,14 @@ public class HashUtil {
     public static String hashPassword(String password, byte[] salt) {
         StringBuilder hashedPassword = new StringBuilder();
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+            MessageDigest messageDigest = MessageDigest.getInstance(HASHING_ALGORITHM);
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
             for (byte b : digest) {
                 hashedPassword.append(String.format("%02x", b));
             }
         } catch (NoSuchAlgorithmException e) {
+            System.out.println("Invalid hashing algorithm " + HASHING_ALGORITHM);
             e.printStackTrace();
         }
         return hashedPassword.toString();
